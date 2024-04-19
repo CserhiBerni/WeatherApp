911a7ec840096ba649bc3e318c70ff54f97ffc90
CserhiBerni <bernat3a@gmail.com>
2024-04-19 09:20:13 +0200

    Created default routing.

--- horse/src/App.tsx
+++ horse/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { Home } from './pages/Home';
+import { Layout } from './pages/Layout';
+import { NoPage } from './pages/NoPage';
 
 function App() {
 
   return (
     <>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
     </>
   )
 }
