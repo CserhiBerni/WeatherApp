2cfa21694b938e273607f15594c8a3abcb84c6db
Petrik Diak <diak@petrik.hu>
2024-04-19 10:09:09 +0200

    Created Navbar Template

--- horse/src/pages/Layout.tsx
+++ horse/src/pages/Layout.tsx
@@ -1,17 +1,20 @@
 import { Outlet, Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../App.css'
 
 export const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+      <Navbar bg="dark" data-bs-theme="dark" expand="lg" className="fixed-top bg-body-tertiary">
+        <Container>
+          <Navbar.Brand className="text-uppercase">lóvé | a lovas web</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link><Link className="home_link" to="/">Home</Link> </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet/>
     </>
   )
 };
