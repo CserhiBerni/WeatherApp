b977e0d9d84a9e23093914c89b011b607c92a40b
CserhiBerni <bernat3a@gmail.com>
2024-04-26 09:27:55 +0200

    Started home page.

--- horse/src/pages/Home.tsx
+++ horse/src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import horse from '../images/gorogvanafaloban.jpg'
+import './Home.css'
+
 export const Home = () => {
     return (
         <div className="home">
-            <h1>Home</h1>
+            <div className="first-section">
+
+            </div>
+            <div className="second-section">
+                <img src={horse} alt="horse" />
+            </div>
         </div>
     );
 };
